844b1e0064bbd46945f14000946f99eb931fb9d2
Carl Bradshaw <carl@dhmedia.com.au>
2017-05-09 16:10:31 +0800

    git init

---
+++ js/space.js
@@ -0,0 +1,86 @@
+(function () {
+    // Set up the scene, camera, and renderer as global variables.
+  var scene, camera, renderer, image, sphere;
+
+  // Sets up the scene.
+  function init() {
+
+    // Create the scene and set the scene size.
+    scene = new THREE.Scene();
+    var WIDTH = window.innerWidth,
+        HEIGHT = window.innerHeight;
+
+    // Create a renderer and add it to the DOM.
+    renderer = new THREE.WebGLRenderer({antialias:true});
+    renderer.setSize(WIDTH, HEIGHT);
+    document.body.appendChild(renderer.domElement);
+
+    // Create a camera, zoom it out from the model a bit, and add it to the scene.
+    camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 20000);
+    camera.position.set(0, 0, 20);
+    scene.add(camera);
+
+    // Create an event listener that resizes the renderer with the browser window.
+    window.addEventListener('resize', function() {
+      var WIDTH = window.innerWidth,
+          HEIGHT = window.innerHeight;
+      renderer.setSize(WIDTH, HEIGHT);
+      camera.aspect = WIDTH / HEIGHT;
+      camera.updateProjectionMatrix();
+    });
+
+    // Set the background color of the scene.
+    renderer.setClearColor(new THREE.Color("rgb(0, 0, 0)"));
+
+    // Create a light, set its position, and add it to the scene.
+    var light = new THREE.PointLight(0xffffff);
+    light.position.set(-100,200,100);
+    scene.add(light);
+
+    image = document.createElement( 'img' );
+    document.body.appendChild( image );
+    var texture = new THREE.Texture( image );
+    image.addEventListener( 'load', function ( event ) { texture.needsUpdate = true; } );
+    image.src = "images/earth.jpg" ;
+
+    var geometry = new THREE.SphereGeometry( 5, 32, 32 );
+    var material = new THREE.MeshPhongMaterial( { map: texture } );
+    sphere = new THREE.Mesh( geometry, material );
+    sphere.position.set(0,0, 0);
+    scene.add( sphere );
+
+
+    // create the geometry sphere
+    var geometry  = new THREE.SphereGeometry(90, 32, 32)
+    // create the material, using a texture of startfield
+    var material  = new THREE.MeshBasicMaterial()
+    material.map   = THREE.ImageUtils.loadTexture('images/stars.png')
+    material.side  = THREE.BackSide
+    // create the mesh based on geometry and material
+    var mesh  = new THREE.Mesh(geometry, material)
+    scene.add( mesh );
+
+
+
+  // Add OrbitControls so that we can pan around with the mouse.
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+  }
+
+  // Renders the scene and updates the render as needed.
+  function animate() {
+
+    // Read more about requestAnimationFrame at http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
+    requestAnimationFrame(animate);
+
+    sphere.rotation.y -= 0.0005;
+    sphere.rotation.x -= 0.0005;
+
+    // Render the scene.
+    renderer.render(scene, camera);
+    controls.update();
+
+  }
+
+  init();
+  animate();
+})();
